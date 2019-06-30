8f213c1ad22cb472c6bcec3377be099c01580d76
jeferson11gns <jeferson11gns@gmail.com>
2019-06-30 17:35:49 -0300

    Codigo correto

--- NEPS/Fita_Colorida.c
+++ NEPS/Fita_Colorida.c
@@ -2,48 +2,39 @@
 
 int main(){
 
-	int n, i, cont = 1;
+	int n, k, i, j, dir = 0, esq = 0;
 	
 	scanf("%d", &n);
 	int fita[n];
 	int aux[n];
 	
 	for(i = 0; i < n; i++) scanf("%d", &fita[i]);
-	
-	for(i = 0; i < n; i++){
-		
-		if(fita[i] == 0){
-			aux[i] = 0;
-			cont = 1;
-			continue;
-		}
-		
-		
-		aux[i] = cont;
-			
-		cont++;
 
-	}
-	cont = 1;
+	for(i = 0; i < n; i++) aux[i] = 100;
 	
-	
-	for(i = n-1; i >= 0; i--){
-		
-		if(aux[i] == 0 || aux[i] == cont){
-			cont = 1;
-			continue;
-		}
-		
-		if(aux[i] != cont){
-			aux[i] = cont;
-			cont++;
+	for(k = 0; k < n; k++){
+		if(fita[k] == 0){
+			aux[k] = 0;
+
+			for(i = k+1; i < n; i++){
+				dir++;
+				if(dir >= 9) dir = 9;
+				if(dir < aux[i]) aux[i] = dir;
+			}
+			dir = 0;
+
+			for(j = k-1; j >= 0; j--){
+				esq++;
+				if(esq >= 9) esq = 9;
+				if(esq < aux[j]) aux[j] = esq;
+			}
+			esq = 0;
 		}
 
 	}
+
 	
-	for(i = 0; i < n; i++) printf("%d", aux[i]);
-	
-		
+	for(i = 0; i < n; i++) printf("%d ", aux[i]);
 
 	return 0;
 }
